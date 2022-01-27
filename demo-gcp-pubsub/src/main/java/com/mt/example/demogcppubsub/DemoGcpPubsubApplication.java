237692552a7e0ac540408c31c0c297bc492033b9
mratyunjayat <64491211+mratyunjayat@users.noreply.github.com>
2022-01-27 11:19:30 -0600

    gcp pubsub v1 demo

---
+++ demo-gcp-pubsub/src/main/java/com/mt/example/demogcppubsub/DemoGcpPubsubApplication.java
@@ -0,0 +1,13 @@
+package com.mt.example.demogcppubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGcpPubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGcpPubsubApplication.class, args);
+	}
+
+}
---
+++ demo-gcp-pubsub/src/test/java/com/mt/example/demogcppubsub/DemoGcpPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.example.demogcppubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGcpPubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
